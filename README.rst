675645ce1adb33bd83b92ee3356b79798bcfb920
Robert Smallshire <rob@sixty-north.com>
2019-05-25 15:18:48 +0200

    Renames mac_display to macos_display

--- README.rst
+++ README.rst
@@ -1,24 +1,24 @@
 ===========
-mac_display
+macos_display
 ===========
 
 Fetch information about macOS displays given a display ID, which is available from
-`nsscreen.deviceDescription()["NSScreenNumber"]` using pyobjc.
+``nsscreen.deviceDescription()["NSScreenNumber"]`` using pyobjc.
 
 Get the user-visible display name from an id::
 
-  >>> import mac_display
-  >>> mac_display.display_name(69732928)
+  >>> import macos_display
+  >>> macos_display.display_name(69732928)
   'Color LCD'
 
 List all display names::
 
   >>> from AppKit import NSScreen
-  >>> import mac_display
+  >>> import macos_display
   >>> screens = NSScreen.screens()
   >>> for screen in screens:
   ...     screen_id = screen.deviceDescription()["NSScreenNumber"]
-  ...     name = mac_display.display_name(screen_id)
+  ...     name = macos_display.display_name(screen_id)
   ...     print(name)
   ... 
   Color LCD
--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ import sys
 import platform
 
 if platform.system() != 'Darwin':
-    print("The mac_display package can only be installed on macOS", file=sys.stderr)
+    print("The macos_display package can only be installed on macOS", file=sys.stderr)
     sys.exit(1)
 
 setup(
-    name='mac_display',
+    name='macos_display',
     author='rob@sixty-north.com',
     author_email="rob@sixty-north.com",
     description="Retrieve information about macOS displays",
     version='1.0',
-    packages = ['mac_display'],
+    packages = ['macos_display'],
     ext_modules=[
         Extension(
-            'mac_display.display',
-            ['mac_display/display_module.c',
-             'mac_display/display.c'],
+            'macos_display.display',
+            ['macos_display/display_module.c',
+             'macos_display/display.c'],
             extra_link_args = [
                 '-framework', 'IOKit',
                 '-framework', 'CoreFoundation',
