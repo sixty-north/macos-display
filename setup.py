621c95d964fbcbdfd5d0310726f6086472729a0e
Robert Smallshire <rob@sixty-north.com>
2021-10-15 10:26:02 +0200

    Bump version: 1.1.4 → 1.1.5

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ setup(
     description="Retrieve information about macOS displays",
     long_description=long_description,
     long_description_content_type='text/x-rst',
-    version='1.1.4',
+    version='1.1.5',
     url="https://github.com/sixty-north/macos-display",
     packages=['macos_display'],
     ext_modules=ext_modules,
